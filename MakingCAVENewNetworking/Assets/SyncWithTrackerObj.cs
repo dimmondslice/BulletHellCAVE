6b35b6194d32cd08b473a7d422a0fe1c51a97c20
spencer johnson <johnss18@rpi.edu>
2016-04-22 16:38:39 -0400

    started cleaning up the foldrs to get ready to package. Also added the sync with tracker object component

---
+++ MakingCAVENewNetworking/Assets/SyncWithTrackerObj.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+//[RequireComponent(typeof(NetworkIdentity))]
+//[RequireComponent(typeof(NetworkTransform))]
+public class SyncWithTrackerObj : MonoBehaviour 
+{
+	public string ER_syncedObjName;
+	public bool ER_syncPosition = true;
+	public bool ER_syncRotation = false;
+
+	private Tracker trackerRef;
+	void Start()
+	{
+		trackerRef = GameObject.FindObjectOfType<Tracker>();
+	}
+	void Update()
+	{
+		//sync the transform.position with the desired tracker obj
+		if(ER_syncPosition)
+		{
+			transform.position = new Vector3(trackerRef.objs[ER_syncedObjName].x,
+											  trackerRef.objs[ER_syncedObjName].y,
+											  trackerRef.objs[ER_syncedObjName].z);
+        }
+		//sync the transform.rotation with the desired tracker obj
+		if (ER_syncRotation)
+		{
+			transform.rotation = Quaternion.Euler(trackerRef.objs[ER_syncedObjName].p,
+													trackerRef.objs[ER_syncedObjName].h,
+													trackerRef.objs[ER_syncedObjName].r);
+		}
+    }
+}
