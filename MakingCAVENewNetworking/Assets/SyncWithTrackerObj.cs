14a2ff4c38b003bebdbf4606f076542943a753df
DomCristaldi <domeniccristaldi@gmail.com>
2016-05-06 12:12:02 -0400

    Error checking
    ++ Added a check to the Tracker Sync component so it doesn't try to grab objects that aren't registered

--- MakingCAVENewNetworking/Assets/SyncWithTrackerObj.cs
+++ MakingCAVENewNetworking/Assets/SyncWithTrackerObj.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿#define DEBUGS_ENABLED
+
+//uncomment to disable debug messages (optimization)
+//#undef DEBUGS_ENABLED
+
+using UnityEngine;
 using UnityEngine.Networking;
 
 //[RequireComponent(typeof(NetworkIdentity))]
@@ -14,21 +19,32 @@ public class SyncWithTrackerObj : MonoBehaviour
 	{
 		trackerRef = GameObject.FindObjectOfType<Tracker>();
 	}
+
 	void Update()
 	{
-		//sync the transform.position with the desired tracker obj
-		if(ER_syncPosition)
-		{
-			transform.position = new Vector3(trackerRef.objs[ER_syncedObjName].x,
-											  trackerRef.objs[ER_syncedObjName].y,
-											  trackerRef.objs[ER_syncedObjName].z);
+        ProcessTracker();	
+    }
+
+    private void ProcessTracker() {
+
+        if (!trackerRef.objs.ContainsKey(ER_syncedObjName)) {
+#if DEBUGS_ENABLED
+            Debug.LogWarningFormat("Supplied object is not registered: {0}", ER_syncedObjName);
+#endif
+            return;
         }
-		//sync the transform.rotation with the desired tracker obj
-		if (ER_syncRotation)
-		{
-			transform.rotation = Quaternion.Euler(trackerRef.objs[ER_syncedObjName].p,
-													trackerRef.objs[ER_syncedObjName].h,
-													trackerRef.objs[ER_syncedObjName].r);
-		}
+
+        //sync the transform.position with the desired tracker obj
+        if (ER_syncPosition) {
+            transform.position = new Vector3(trackerRef.objs[ER_syncedObjName].x,
+                                              trackerRef.objs[ER_syncedObjName].y,
+                                              trackerRef.objs[ER_syncedObjName].z);
+        }
+        //sync the transform.rotation with the desired tracker obj
+        if (ER_syncRotation) {
+            transform.rotation = Quaternion.Euler(trackerRef.objs[ER_syncedObjName].p,
+                                                    trackerRef.objs[ER_syncedObjName].h,
+                                                    trackerRef.objs[ER_syncedObjName].r);
+        }
     }
 }
